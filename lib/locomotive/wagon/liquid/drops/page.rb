b2ebdb0305d04b7690ca63713938488f8a8c389b
Julian Berger <jb@confluentes.com>
2014-03-02 14:55:38 +0100

    Expose position in page liquid drop

--- lib/locomotive/wagon/liquid/drops/page.rb
+++ lib/locomotive/wagon/liquid/drops/page.rb
@@ -4,7 +4,7 @@ module Locomotive
       module Drops
         class Page < Base
 
-          delegate :title, :slug, :fullpath, :parent, :depth, :seo_title, :redirect_url, :meta_description, :meta_keywords,
+          delegate :position, :title, :slug, :fullpath, :parent, :depth, :seo_title, :redirect_url, :meta_description, :meta_keywords,
                    :templatized?, :published?, :redirect?, :listed?, :handle, to: :@_source
 
           def children
